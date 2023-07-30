4babb0280f36599eeb1060028c128c504b6ca8a2
saurabh <saurabhnirwan11@gmail.com>
2023-07-30 19:12:59 +0530

    Added logistic regression in training.ipynb

--- app.py
+++ app.py
@@ -11,6 +11,17 @@ ohe = pickle.load(open("models/credit_score_multi_class_ohe_encoder.pkl","rb"))
 le = pickle.load(open("models/credit_score_multi_class_le.pkl","rb"))
 dummy = pickle.load(open("models/credit_score_multi_class_get_dummy.pkl","rb"))
 
-@app.route('/')
+@app.route("/")
 def home():
-    return render_template('home.html')
+    return render_template("home.html")
+
+@app.route("/predict",methods=["POST"])
+def predict():
+    data=[float(x) for x in request.form.values()]
+    final_input=scalar.transform(np.array(data).reshape(1,-1))
+    print(final_input)
+    output=rfc_model.predict(final_input)[0]
+    return render_template("home.html",prediction_text="The House price prediction is {}".format(output))
+
+if __name__=="__main__":
+    app.run(debug=True)
